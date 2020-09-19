c3ace8a89b4463c76202f49379c0276820a1932d
Iryna Stepaniuk <Iryna_Stepaniuk@epam.com>
2020-09-19 19:39:39 +0300

    second change hello.cs

--- forMerge/HelloName.cs
+++ forMerge/HelloName.cs
@@ -11,6 +11,7 @@ namespace HelloName
             Console.WriteLine("Hello Stepaniuk!");
             Console.WriteLine("For hello_Stepaniuk!");
             Console.WriteLine("1st line");
+            Console.WriteLine("2nd line");
 
         }
     }
