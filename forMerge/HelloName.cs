aa3e25f4c685aad516db42ce9a51162fb877ce21
Iryna Stepaniuk <Iryna_Stepaniuk@epam.com>
2020-09-19 19:11:06 +0300

    add folder forMerge

---
+++ forMerge/HelloName.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello <Name>!");
+        }
+    }
+}
